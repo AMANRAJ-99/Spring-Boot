eb86212186645be28001a95ecc21e4e7d5753c7e
aman <aman.raj@eshopbox.com>
2022-04-16 20:51:29 +0530

    committed

---
+++ java/com/Springboot/sbnew/SbnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Springboot.sbnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ resources/templates/TestWebPage.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Welcome to New Web-page</title>
+</head>
+<body>
+<p>
+Credentials Verified
+You have been SuccessFully Logged In
+</p>
+</body>
+</html>
