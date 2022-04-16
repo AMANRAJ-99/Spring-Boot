841c7e1e7017a8ac92627c85b7f2d0a2b137dca2
aman <aman.raj@eshopbox.com>
2022-04-16 20:43:13 +0530

    committed

---
+++ src/com/Springboot/sbnew/application/SbnewApplication.java
@@ -0,0 +1,14 @@
+package com.Springboot.sbnew.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbnewApplication.class, args);
+		
+	}
+
+}
---
+++ src/com/Springboot/sbnew/model/Model.java
@@ -0,0 +1,14 @@
+package com.Springboot.sbnew.model;
+
+public class Model {
+
+	private String Hello;
+
+	public String getHello() {
+		return Hello;
+	}
+
+	public void setHello(String hello) {
+		this.Hello = hello;
+	}
+}
